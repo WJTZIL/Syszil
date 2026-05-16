ae27bf9d8e7b1b9a4d01eb943f654693f6e266b3
Wojciech <matuszak.wojciech02.11@gmail.com>
2026-05-16 04:28:37 -0400

    Add files via upload

--- Syszil.bat
+++ Syszil.bat
@@ -1,5 +1,23 @@
 @echo off
-title Syszil 2.0
+title Syszil 3.0
+goto check_Permissions
+
+:check_Permissions
+    echo Detecting permissions...
+    
+    net session >nul 2>&1
+    if %errorLevel% == 0 (
+	color 4
+        echo Administrative permissions detected. Can't run Syszil.
+	pause
+	exit
+    ) else (
+        goto System
+    )
+    
+    pause >nul
+
+:System
 echo Finding a kernel...
 echo Loading syszil.dll...
 pause
@@ -7,30 +25,37 @@ echo Loaded!
 pause
 
 cls
-title Syszil 2.0 Setup
-echo Welcome to Syszil 2.0 Setup!
+title Syszil 3.0 Setup
+echo Welcome to Syszil 3.0 Setup!
 echo Please enter your username
 set /p account=
 echo Loading the console...
 pause
-echo Welcome to Syszil 2.0, %account%.
+echo Welcome to Syszil 3.0, %account%.
 pause
-goto :menu
+goto menu
 
 :menu
 cls
-title Syszil 2.0 Desktop
+title Syszil 3.0 Desktop
 echo What Applications Do you want to lanuch?
 echo 1. System Info
 echo 2. RD-1 (Sandboxed Taskmgr (with access to PC))
+echo 3. Syszil Browser (yours)
 
 set /p input=
 if %input% == 1 goto info
 if %input% == 2 goto rd-1
+if %input% == 3 goto browser
+
+:browser
+start "" "http://www.google.com"
+goto menu
+
 
 :rd-1
 start taskmgr -d
-goto :menu
+goto menu
 
 :info
 
@@ -40,7 +65,7 @@ echo     SYSZIL
 echo.=======================
 echo     DETAILS  
 echo.
-echo     VERSION = 2.0
+echo     VERSION = 3.0
 echo.     
 echo     RAM = 4GB
 echo.
